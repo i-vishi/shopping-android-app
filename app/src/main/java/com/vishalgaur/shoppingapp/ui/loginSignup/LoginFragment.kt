62cf6f1b962acf380fd61a42196135c2c515b58d
i-vishi <rr.vishalgaur@gmail.com>
2021-04-15 15:11:06 +0530

    project init

---
+++ app/src/main/java/com/vishalgaur/shoppingapp/ui/loginSignup/LoginFragment.kt
@@ -0,0 +1,27 @@
+package com.vishalgaur.shoppingapp.ui.loginSignup
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.vishalgaur.shoppingapp.R
+
+class LoginFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+}
---
+++ app/src/main/java/com/vishalgaur/shoppingapp/ui/loginSignup/LoginSignupActivity.kt
@@ -0,0 +1,12 @@
+package com.vishalgaur.shoppingapp.ui.loginSignup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.vishalgaur.shoppingapp.R
+
+class LoginSignupActivity : AppCompatActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_signup)
+	}
+}
