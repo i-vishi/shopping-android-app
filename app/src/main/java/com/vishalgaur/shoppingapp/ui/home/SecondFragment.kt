224f1649ec15d90545a202d107b7146b04059238
i-vishi <rr.vishalgaur@gmail.com>
2021-04-15 15:18:20 +0530

    removed unused code

--- app/src/main/java/com/vishalgaur/shoppingapp/ui/home/SecondFragment.kt
+++ app/src/main/java/com/vishalgaur/shoppingapp/ui/home/SecondFragment.kt
@@ -21,12 +21,4 @@ class SecondFragment : Fragment() {
 		// Inflate the layout for this fragment
 		return inflater.inflate(R.layout.fragment_second, container, false)
 	}
-
-	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-		super.onViewCreated(view, savedInstanceState)
-
-		view.findViewById<Button>(R.id.button_second).setOnClickListener {
-			findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
-		}
-	}
 }
