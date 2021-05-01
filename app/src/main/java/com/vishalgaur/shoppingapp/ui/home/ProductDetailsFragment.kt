0faee53f362008a2a985f97937bb05d92a72e7ef
i-vishi <rr.vishalgaur@gmail.com>
2021-05-01 19:08:03 +0530

    added radio buttons for size

--- app/src/main/java/com/vishalgaur/shoppingapp/ui/home/ProductDetailsFragment.kt
+++ app/src/main/java/com/vishalgaur/shoppingapp/ui/home/ProductDetailsFragment.kt
@@ -1,15 +1,22 @@
 package com.vishalgaur.shoppingapp.ui.home
 
 import android.annotation.SuppressLint
+import android.app.ActionBar
 import android.app.Application
 import android.content.res.ColorStateList
 import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Typeface
 import android.os.Bundle
 import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import androidx.core.content.ContextCompat
+import androidx.core.view.setMargins
+import androidx.core.view.setPadding
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
@@ -80,8 +87,8 @@ class ProductDetailsFragment : Fragment() {
                 R.string.pro_details_price_value,
                 viewModel.productData.value?.price.toString()
         )
-		setShoeSizeChips()
-		setShoeColorsChips()
+		setShoeSizeButtons()
+//		setShoeColorsChips()
 		binding.proDetailsSpecificsText.text = viewModel.productData.value?.description ?: ""
 	}
 
@@ -115,78 +122,103 @@ class ProductDetailsFragment : Fragment() {
 	}
 
 	@SuppressLint("ResourceAsColor")
-	private fun setShoeSizeChips() {
-		binding.proDetailsSizesChipGroup.apply {
-			for ((_, v) in ShoeSizes) {
-				if (viewModel.productData.value?.availableSizes?.contains(v) == true) {
-					val chip = Chip(context)
-					chip.id = v
-					chip.tag = v
-					chip.text = "$v"
+	private fun setShoeSizeButtons() {
 
-					chip.chipStrokeColor =
-							ColorStateList.valueOf(ContextCompat.getColor(context, R.color.gray))
-					chip.checkedIcon = null
-					chip.chipStrokeWidth = TypedValue.applyDimension(
-                            TypedValue.COMPLEX_UNIT_DIP,
-                            1F,
-                            context.resources.displayMetrics
-                    )
-					chip.isCheckable = true
-					chip.isEnabled = true
-					chip.setTextColor(Color.BLACK)
-					chip.chipBackgroundColor = ColorStateList.valueOf(Color.TRANSPARENT)
-					chip.setOnCheckedChangeListener { _, isChecked ->
-						if (isChecked) {
-							chip.chipStrokeColor = ColorStateList.valueOf(
-                                    ContextCompat.getColor(
-                                            context,
-                                            R.color.blue_accent_300
-                                    )
-                            )
-						} else {
-							chip.chipStrokeColor = ColorStateList.valueOf(
-                                    ContextCompat.getColor(
-                                            context,
-                                            R.color.gray
-                                    )
-                            )
-						}
-					}
-
-					addView(chip)
+		binding.proDetailsSizesRadioGroup.apply {
+			for((_, v) in ShoeSizes) {
+				if(viewModel.productData.value?.availableSizes?.contains(v) == true) {
+					val radioButton = RadioButton(context)
+					radioButton.id = v
+					radioButton.tag = v
+					radioButton.setPadding(resources.getDimensionPixelSize(R.dimen.radio_padding_size))
+					val param = binding.proDetailsSizesRadioGroup.layoutParams as ViewGroup.MarginLayoutParams
+					param.setMargins(resources.getDimensionPixelSize(R.dimen.radio_margin_size))
+					param.width = ViewGroup.LayoutParams.WRAP_CONTENT
+					param.height = ViewGroup.LayoutParams.WRAP_CONTENT
+					radioButton.layoutParams = param
+					radioButton.background = ContextCompat.getDrawable(context, R.drawable.radio_selector)
+					radioButton.setButtonDrawable(R.color.transparent)
+					radioButton.setTextSize(TypedValue.COMPLEX_UNIT_SP, 14F)
+					radioButton.setTextColor(R.color.black)
+					radioButton.setTypeface(null, Typeface.BOLD)
+					radioButton.textAlignment = View.TEXT_ALIGNMENT_CENTER
+					radioButton.text = "$v"
+					addView(radioButton)
 				}
 			}
 			invalidate()
 		}
+//		binding.proDetailsSizesChipGroup.apply {
+//			for ((_, v) in ShoeSizes) {
+//				if (viewModel.productData.value?.availableSizes?.contains(v) == true) {
+//					val chip = Chip(context)
+//					chip.id = v
+//					chip.tag = v
+//					chip.text = "$v"
+//
+//					chip.chipStrokeColor =
+//							ColorStateList.valueOf(ContextCompat.getColor(context, R.color.gray))
+//					chip.checkedIcon = null
+//					chip.chipStrokeWidth = TypedValue.applyDimension(
+//                            TypedValue.COMPLEX_UNIT_DIP,
+//                            1F,
+//                            context.resources.displayMetrics
+//                    )
+//					chip.isCheckable = true
+//					chip.isEnabled = true
+//					chip.setTextColor(Color.BLACK)
+//					chip.chipBackgroundColor = ColorStateList.valueOf(Color.TRANSPARENT)
+//					chip.setOnCheckedChangeListener { _, isChecked ->
+//						if (isChecked) {
+//							chip.chipStrokeColor = ColorStateList.valueOf(
+//                                    ContextCompat.getColor(
+//                                            context,
+//                                            R.color.blue_accent_300
+//                                    )
+//                            )
+//						} else {
+//							chip.chipStrokeColor = ColorStateList.valueOf(
+//                                    ContextCompat.getColor(
+//                                            context,
+//                                            R.color.gray
+//                                    )
+//                            )
+//						}
+//					}
+//
+//					addView(chip)
+//				}
+//			}
+//			invalidate()
+//		}
 	}
 
-	private fun setShoeColorsChips() {
-		binding.proDetailsColorsChipGroup.apply {
-			var ind = 1
-			for ((k, v) in ShoeColors) {
-				if (viewModel.productData.value?.availableColors?.contains(k) == true) {
-					val chip = Chip(context)
-					chip.id = ind
-					chip.tag = k
-					chip.text = ".."
-					chip.setTextColor(ColorStateList.valueOf(Color.parseColor(v)))
-					chip.chipStrokeColor =
-							ColorStateList.valueOf(ContextCompat.getColor(context, R.color.blue_accent_300))
-					chip.chipStrokeWidth = TypedValue.applyDimension(
-                            TypedValue.COMPLEX_UNIT_DIP,
-                            1F,
-                            context.resources.displayMetrics
-                    )
-					chip.chipBackgroundColor = ColorStateList.valueOf(Color.parseColor(v))
-					chip.isEnabled = true
-					chip.isCheckable = true
-					addView(chip)
-					ind++
-				}
-			}
-			invalidate()
-		}
-	}
+//	private fun setShoeColorsChips() {
+//		binding.proDetailsColorsChipGroup.apply {
+//			var ind = 1
+//			for ((k, v) in ShoeColors) {
+//				if (viewModel.productData.value?.availableColors?.contains(k) == true) {
+//					val chip = Chip(context)
+//					chip.id = ind
+//					chip.tag = k
+//					chip.text = ".."
+//					chip.setTextColor(ColorStateList.valueOf(Color.parseColor(v)))
+//					chip.chipStrokeColor =
+//							ColorStateList.valueOf(ContextCompat.getColor(context, R.color.blue_accent_300))
+//					chip.chipStrokeWidth = TypedValue.applyDimension(
+//                            TypedValue.COMPLEX_UNIT_DIP,
+//                            1F,
+//                            context.resources.displayMetrics
+//                    )
+//					chip.chipBackgroundColor = ColorStateList.valueOf(Color.parseColor(v))
+//					chip.isEnabled = true
+//					chip.isCheckable = true
+//					addView(chip)
+//					ind++
+//				}
+//			}
+//			invalidate()
+//		}
+//	}
 
 }
