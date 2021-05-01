f0e2ea8ae831004e5e56d7df865961ae1cac9407
i-vishi <rr.vishalgaur@gmail.com>
2021-05-01 23:32:31 +0530

    added action icons in app bar

--- app/src/main/java/com/vishalgaur/shoppingapp/ui/home/ProductDetailsFragment.kt
+++ app/src/main/java/com/vishalgaur/shoppingapp/ui/home/ProductDetailsFragment.kt
@@ -3,7 +3,6 @@ package com.vishalgaur.shoppingapp.ui.home
 import android.app.Application
 import android.content.res.ColorStateList
 import android.graphics.Color
-import android.graphics.ColorFilter
 import android.graphics.PorterDuff
 import android.graphics.Typeface
 import android.os.Bundle
@@ -14,7 +13,6 @@ import android.view.ViewGroup
 import android.widget.RadioButton
 import androidx.core.content.ContextCompat
 import androidx.core.view.setMargins
-import androidx.core.view.setPadding
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
