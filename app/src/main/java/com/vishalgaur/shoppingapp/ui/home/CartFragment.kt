6e377655885c18844e14d9ed5893ce5f3bca7f60
i-vishi <rr.vishalgaur@gmail.com>
2021-05-12 14:23:42 +0530

    added price card to cart fragment

--- app/src/main/java/com/vishalgaur/shoppingapp/ui/home/CartFragment.kt
+++ app/src/main/java/com/vishalgaur/shoppingapp/ui/home/CartFragment.kt
@@ -5,12 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.vishalgaur.shoppingapp.R
+import com.vishalgaur.shoppingapp.data.UserData
+import com.vishalgaur.shoppingapp.data.utils.StoreDataStatus
 import com.vishalgaur.shoppingapp.databinding.FragmentCartBinding
+import com.vishalgaur.shoppingapp.viewModels.OrderViewModel
 
-class CartFragment: Fragment() {
+class CartFragment : Fragment() {
+
 	private lateinit var binding: FragmentCartBinding
+	private val orderViewModel: OrderViewModel by activityViewModels()
 
 	override fun onCreateView(
 		inflater: LayoutInflater,
@@ -20,7 +26,41 @@ class CartFragment: Fragment() {
 		binding = FragmentCartBinding.inflate(layoutInflater)
 
 		setViews()
+		setObservers()
+
 		return binding.root
+	}
+
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+
+		orderViewModel.getCartItems()
+
+		orderViewModel.cartItems.observe(viewLifecycleOwner) { itemList ->
+			// set adapter and recyclerview
+
+			if (itemList.isNotEmpty()) {
+				setPriceCard(itemList)
+			}
+		}
+	}
+
+	private fun setObservers() {
+		orderViewModel.dataStatus.observe(viewLifecycleOwner) { status ->
+			when (status) {
+				StoreDataStatus.LOADING -> {
+					binding.loaderLayout.circularLoader.visibility = View.VISIBLE
+					binding.loaderLayout.circularLoader.showAnimationBehavior
+				}
+				else -> {
+					binding.loaderLayout.circularLoader.hideAnimationBehavior
+					binding.loaderLayout.circularLoader.visibility = View.GONE
+				}
+			}
+		}
+		orderViewModel.priceList.observe(viewLifecycleOwner) {
+			orderViewModel.cartItems.value?.let { it1 -> setPriceCard(it1) }
+		}
 	}
 
 	private fun setViews() {
@@ -29,6 +69,17 @@ class CartFragment: Fragment() {
 		binding.cartCheckOutBtn.setOnClickListener {
 			navigateToSelectAddress()
 		}
+	}
+
+	private fun setPriceCard(itemList: List<UserData.CartItem>) {
+		binding.cartPriceCardLayout.priceItemsLabelTv.text =
+			getString(R.string.price_card_items_string, itemList.size.toString())
+		binding.cartPriceCardLayout.priceItemsAmountTv.text =
+			getString(R.string.price_text, orderViewModel.getItemsPriceTotal().toString())
+		binding.cartPriceCardLayout.priceShippingAmountTv.text = getString(R.string.price_text, "0")
+		binding.cartPriceCardLayout.priceChargesAmountTv.text = getString(R.string.price_text, "0")
+		binding.cartPriceCardLayout.priceTotalAmountTv.text =
+			getString(R.string.price_text, orderViewModel.getItemsPriceTotal().toString())
 	}
 
 	private fun navigateToSelectAddress() {
--- app/src/main/java/com/vishalgaur/shoppingapp/viewModels/OrderViewModel.kt
+++ app/src/main/java/com/vishalgaur/shoppingapp/viewModels/OrderViewModel.kt
@@ -6,7 +6,8 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
-import com.vishalgaur.shoppingapp.data.Result.*
+import com.vishalgaur.shoppingapp.data.Result.Error
+import com.vishalgaur.shoppingapp.data.Result.Success
 import com.vishalgaur.shoppingapp.data.ShoppingAppSessionManager
 import com.vishalgaur.shoppingapp.data.UserData
 import com.vishalgaur.shoppingapp.data.source.repository.AuthRepository
@@ -28,8 +29,44 @@ class OrderViewModel(application: Application) : AndroidViewModel(application) {
 	private val _userAddresses = MutableLiveData<List<UserData.Address>>()
 	val userAddresses: LiveData<List<UserData.Address>> get() = _userAddresses
 
+	private val _cartItems = MutableLiveData<List<UserData.CartItem>>()
+	val cartItems: LiveData<List<UserData.CartItem>> get() = _cartItems
+
+	private val _priceList = MutableLiveData<Map<String, Double>>()
+	val priceList: LiveData<Map<String, Double>> get() = _priceList
+
 	private val _dataStatus = MutableLiveData<StoreDataStatus>()
 	val dataStatus: LiveData<StoreDataStatus> get() = _dataStatus
+
+	init {
+		getCartItems()
+	}
+
+	fun getCartItems() {
+		Log.d(TAG, "Getting Cart Items")
+		_dataStatus.value = StoreDataStatus.LOADING
+		viewModelScope.launch {
+			val deferredRes = async { authRepository.getUserData(currentUser!!) }
+			val userRes = deferredRes.await()
+			if (userRes is Success) {
+				val uData = userRes.data
+				if (uData != null) {
+					_cartItems.value = uData.cart
+					val priceRes = async { getAllItemsPrice() }
+					priceRes.await()
+					Log.d(TAG, "Getting Cart Items: Success ${_priceList.value}")
+				} else {
+					_cartItems.value = emptyList()
+					_dataStatus.value = StoreDataStatus.ERROR
+					Log.d(TAG, "Getting Cart Items: User Not Found")
+				}
+			} else {
+				_cartItems.value = emptyList()
+				_dataStatus.value = StoreDataStatus.ERROR
+				Log.d(TAG, "Getting Cart Items: Error Occurred")
+			}
+		}
+	}
 
 	fun getUserAddresses() {
 		Log.d(TAG, "Getting Addresses")
@@ -57,6 +94,39 @@ class OrderViewModel(application: Application) : AndroidViewModel(application) {
 				is Error -> Log.d(TAG, "onDeleteAddress: Error, ${res.exception}")
 				else -> Log.d(TAG, "onDeleteAddress: Some error occurred!")
 			}
+		}
+	}
+
+	fun getItemsPriceTotal(): Double {
+		return _priceList.value?.values?.sum() ?: 0.0
+	}
+
+	private suspend fun getAllItemsPrice() {
+		viewModelScope.launch {
+			_dataStatus.value = StoreDataStatus.LOADING
+			val priceMap = mutableMapOf<String, Double>()
+			var res = true
+			_cartItems.value?.let { itemList ->
+				itemList.forEach label@{ item ->
+					val productDeferredRes = async {
+						productsRepository.getProductById(item.productId, true)
+					}
+					val proRes = productDeferredRes.await()
+					if (proRes is Success) {
+						val proData = proRes.data
+						priceMap[item.itemId] = proData.price
+					} else {
+						res = false
+						return@label
+					}
+				}
+			}
+			if (!res) {
+				_dataStatus.value = StoreDataStatus.ERROR
+			} else {
+				_dataStatus.value = StoreDataStatus.DONE
+			}
+			_priceList.value = priceMap
 		}
 	}
 }
