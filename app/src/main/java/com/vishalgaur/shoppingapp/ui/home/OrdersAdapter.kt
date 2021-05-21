c9bd95ca18185f93ed5ef4824c1cfa39be6db985
i-vishi <rr.vishalgaur@gmail.com>
2021-05-21 13:11:20 +0530

    added orders adapter

---
+++ app/src/main/java/com/vishalgaur/shoppingapp/ui/home/OrdersAdapter.kt
@@ -0,0 +1,55 @@
+package com.vishalgaur.shoppingapp.ui.home
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.vishalgaur.shoppingapp.R
+import com.vishalgaur.shoppingapp.data.UserData
+import com.vishalgaur.shoppingapp.databinding.LayoutOrderSummaryCardBinding
+
+class OrdersAdapter(private val data: List<UserData.OrderItem>, private val context: Context) :
+	RecyclerView.Adapter<OrdersAdapter.ViewHolder>() {
+
+	lateinit var onClickListener: OnClickListener
+
+	inner class ViewHolder(private val binding: LayoutOrderSummaryCardBinding) :
+		RecyclerView.ViewHolder(binding.root) {
+		fun bind(orderData: UserData.OrderItem) {
+			binding.orderSummaryCard.setOnClickListener { onClickListener.onCardClick(orderData.orderId) }
+			binding.orderSummaryIdTv.text = orderData.orderId
+			binding.orderSummaryDateTv.text =
+				context.getString(R.string.order_date_text, orderData.orderDate.toString())
+			binding.orderSummaryStatusValueTv.text = orderData.status
+			val totalItems = orderData.items.map { it.quantity }.sum()
+			binding.orderSummaryItemsCountTv.text =
+				context.getString(R.string.order_items_count_text, totalItems.toString())
+			var totalAmount = 0.0
+			orderData.itemsPrices.forEach { (itemId, price) ->
+				totalAmount += price * (orderData.items.find { it.itemId == itemId }?.quantity ?: 1)
+			}
+			binding.orderSummaryTotalAmountTv.text =
+				context.getString(R.string.price_text, totalAmount.toString())
+		}
+	}
+
+	override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+		return ViewHolder(
+			LayoutOrderSummaryCardBinding.inflate(
+				LayoutInflater.from(parent.context),
+				parent,
+				false
+			)
+		)
+	}
+
+	override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+		holder.bind(data[position])
+	}
+
+	override fun getItemCount() = data.size
+
+	interface OnClickListener {
+		fun onCardClick(orderId: String)
+	}
+}
--- app/src/main/java/com/vishalgaur/shoppingapp/ui/home/OrdersFragment.kt
+++ app/src/main/java/com/vishalgaur/shoppingapp/ui/home/OrdersFragment.kt
@@ -1,6 +1,42 @@
 package com.vishalgaur.shoppingapp.ui.home
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.vishalgaur.shoppingapp.R
+import com.vishalgaur.shoppingapp.databinding.FragmentOrdersBinding
 
 class OrdersFragment: Fragment() {
+
+	private lateinit var binding: FragmentOrdersBinding
+	private lateinit var ordersAdapter: OrdersAdapter
+
+	override fun onCreateView(
+		inflater: LayoutInflater,
+		container: ViewGroup?,
+		savedInstanceState: Bundle?
+	): View? {
+		binding = FragmentOrdersBinding.inflate(layoutInflater)
+
+		setViews()
+		setObservers()
+
+		return binding.root
+	}
+
+	private fun setViews() {
+		binding.loaderLayout.circularLoader.visibility = View.GONE
+		binding.ordersAppBar.topAppBar.title = getString(R.string.orders_fragment_title)
+		binding.ordersEmptyTextView.visibility = View.GONE
+	if(context != null) {
+		ordersAdapter = OrdersAdapter(emptyList(), requireContext())
+		binding.orderAllOrdersRecyclerView.adapter = ordersAdapter
+	}
+	}
+
+	private fun setObservers() {
+
+	}
 }
